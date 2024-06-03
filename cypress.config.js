1d156b0d0d11ff35c5fdbe01a1da873a32d14ff2
sarikasp <pansaresarika98@gmail.com>
2024-06-03 11:44:04 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  // projectId: 'cib56p',
+  projectId: "9uujyu",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
+  
